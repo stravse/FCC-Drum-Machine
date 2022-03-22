57fa71dd5fe1845aa970f545e33401e1d9d86a79
stravse <stravse@gmail.com>
2022-03-23 01:49:39 +0800

    made the change in color of the sound switch

--- src/components/displaySwitchSound.js
+++ src/components/displaySwitchSound.js
@@ -2,7 +2,20 @@ import React from "react";
 import "../scss/display.scss";
 
 class DisplaySwitchSound extends React.Component{
-
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        const switchButton = document.getElementById("switch-button");
+        this.props.changeSound();
+        if(switchButton.classList.contains("power-on")){
+            switchButton.classList.replace('power-on', "power-off");
+        } else {
+            switchButton.classList.replace('power-off', 'power-on');
+        }
+        
+    }
 
     render(){
         return(
@@ -10,7 +23,7 @@ class DisplaySwitchSound extends React.Component{
                 <input className="volume" onChange={this.props.changeVolume} type="range" max="100" min="0" step="1" />
                 <div className="sound-container">
                     <div className="display-text">CHANGE SOUND</div>
-                    <button id="switch-button" className="switch-button" onClick={this.props.changeSound} >{this.props.soundBoard1? "Sound 2": "Sound 1"}</button>
+                    <button id="switch-button" className="switch-button power-on" onClick={this.handleClick} >{this.props.soundBoard1? "Sound 2": "Sound 1"}</button>
                 </div>
                 
             </div>
